62518ce02e7dc1f87ae4b7368311a9e3cd514ba1
Agnieszka Pilch <agnieszka.pilch.ext@ecx.io>
2021-05-23 19:57:14 +0200

    Initialize project using Create React App

--- src/test/UserExchangeList.test.js
+++ src/test/UserExchangeList.test.js
@@ -1,10 +1,10 @@
 import { render, screen , fireEvent} from '@testing-library/react';
 import UserExchangeList from './../components/UserExchangeList';
+const calc = [{timestamp:new Date().getTime(),calculation:1.2 , currency: 'EUR', rate: 1, value: 1}]
 beforeAll(() => {
-    window.localStorage.setItem('calculations',[{timestamp:new Date().getTime(),calculation:1.2 , currency: 'EUR', rate: 1, value: 1}])})
+    window.localStorage.setItem('calculations',calc)})
 
 test('User list componenent',() => {
-    render(<UserExchangeList items={[{timestamp:new Date().getTime(),calculation:1.2 , currency: 'EUR', rate: 1, value: 1}]}/>)
-    // await fireEvent.click(screen.getByText(/Delete/i))
+    render(<UserExchangeList items={calc}/>)
     expect(screen.getByText('Currency')).toBeInTheDocument()
 })
